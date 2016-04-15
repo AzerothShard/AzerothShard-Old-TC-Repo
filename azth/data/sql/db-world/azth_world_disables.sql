6d0f43bd561307627e0ef509982b35615ca85075
Yehonal <hw.2@hotmail.it>
2016-04-15 10:30:17 +0200

    removed all custom sql from azth
    Conflicts:
    	azth/data/sql/db-auth/azth_auth_rbac.sql

--- azth/data/sql/db-world/azth_world_disables.sql
+++
@@ -1,3 +0,0 @@
-# fix for TOC and mmaps
-REPLACE INTO disables VALUES (7, 650, 0, '', '', 'Mmaps - Trial of the Champion');
-REPLACE INTO disables VALUES (7, 649, 0, '', '', 'Mmaps - Trial of the Crusader');
--- azth/data/sql/tools/azth_auth_clean.sql
+++
@@ -1,5 +0,0 @@
-# remove approved accounts from banlist
-
-DELETE FROM account_banned WHERE banreason = "New user waiting for approvement" AND active = 0;
-
-
